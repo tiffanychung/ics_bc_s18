2c5e14c3eedeeef9343029f26206854b7453f023
tiffchung1 <40221421+tiffchung1@users.noreply.github.com>
2018-06-14 15:54:48 -0700

    Update minutes_in_decade.rb

--- week1/ch02/minutes_in_decade.rb
+++ week1/ch02/minutes_in_decade.rb
@@ -1,5 +1,4 @@
-mins_in_year = hours_in_year * 60
-
+mins_in_year = 365.25*60 * 60
 mins_in_decade = mins_in_year * 10
-
-puts "There are " + separate_comma(mins_in_decade) + " minutes in a decade."
+puts 'Number of mins in decade'
+puts mins_in_decade
