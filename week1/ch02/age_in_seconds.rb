388d38656ce40fd43e78084cff8c860d383d164b
Your Name <you@example.com>
2018-06-29 13:00:13 -0700

    Fetch week3

--- week1/ch02/age_in_seconds.rb
+++ week1/ch02/age_in_seconds.rb
@@ -1,2 +1,5 @@
 secs_in_year = 365.25*24*60*60
-puts 'How many seconds old I am' + (secs_in_year*18)
+
+puts 'How many seconds old I am: '
+
+puts (secs_in_year*18)
---
+++ week1/ch02/biggernumscratch.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+oldfavnumber = gets.chomp
+newfavnumber = oldfavnumber.to_i + 1
+puts "That's lame dude! I think your new favorite number should be "+ newfavnumber.to_s + " since it's bigger and better!"
